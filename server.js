57d3b15337369b2dc558519d3f8a90923869f7a5
Beharasaikumar <163816273+Beharasaikumar@users.noreply.github.com>
2025-05-30 12:38:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const express = require("express");
    const app = express();
 
    app.use(cors({
-    origin: 'http://localhost:5173', // Adjust to your frontend port
+    origin: 'https://saikumarr.netlify.app', // Adjust to your frontend port
     methods: ['GET', 'POST'],
     credentials: true
   }));
